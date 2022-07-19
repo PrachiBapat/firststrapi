3a1c52106c23601ab3dd154989b678cf8451cbd6
prachi bapat <prachibapat.21@gmail.com>
2022-07-20 03:07:22 +0530

    prachi

--- config/database.js
+++ config/database.js
@@ -1,11 +1,32 @@
-const path = require('path');
+// const path = require('path');
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'sqlite',
+//     connection: {
+//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
+
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'mysql',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env('DATABASE_HOST', '0.0.0.0'),
+      port: env.int('DATABASE_PORT', '3306'),
+      database: env('DATABASE_NAME', 'strapidb'),
+      user: env('DATABASE_USERNAME', 'root'),
+      password: env('DATABASE_PASSWORD', ''),
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+      },
     },
-    useNullAsDefault: true,
+    debug: false,
   },
 });
+
+// (host and port)
+
